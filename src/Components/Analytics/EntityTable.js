6ca463e1e11b4a8b2c0392bc03c3471d2c8b8147
Kunal-2001 <kunal999936@gmail.com>
2021-08-23 19:17:27 +0530

    Chart Integration

--- src/Components/Analytics/EntityTable.js
+++ src/Components/Analytics/EntityTable.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { AgGridColumn, AgGridReact } from "ag-grid-react";
 import IconButton from "@material-ui/core/IconButton";
 import AssessmentIcon from "@material-ui/icons/Assessment";
+import PieChart from "./PieChart";
 
 import "./EntityTable.css";
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 
 const EntityTable = () => {
+  const [pieChart, setPieChart] = useState(false);
   const rowData = [
-    { Score: "Toyota", Count: "Celica" },
-    { Score: "Ford", Count: "Mondeo" },
-    { Score: "Porsche", Count: "Boxter" },
+    { Score: "Toyota", Count: 10 },
+    { Score: "Ford", Count: 33 },
+    { Score: "Porsche", Count: 67 },
   ];
+
+  const showPieChart = () => {
+    setPieChart(true);
+  };
 
   const defaultColDefs = {
     sortable: true,
@@ -21,6 +27,7 @@ const EntityTable = () => {
   };
   return (
     <div className="table-container">
+      {pieChart && <PieChart data={rowData} setPieChart={setPieChart} />}
       <div className="ag-theme-alpine" style={{ height: 450, width: 400 }}>
         <AgGridReact
           defaultColDef={defaultColDefs}
@@ -34,7 +41,7 @@ const EntityTable = () => {
         </AgGridReact>
       </div>
       <div className="chart-button">
-        <IconButton aria-label="delete" size="medium">
+        <IconButton onClick={showPieChart} aria-label="delete" size="medium">
           <AssessmentIcon fontSize="inherit" />
         </IconButton>
       </div>
--- src/Components/Analytics/PieChart.js
+++ src/Components/Analytics/PieChart.js
@@ -1,103 +1,72 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
+import React, { useEffect, useState } from "react";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
-import MuiDialogTitle from "@material-ui/core/DialogTitle";
-import MuiDialogContent from "@material-ui/core/DialogContent";
-import MuiDialogActions from "@material-ui/core/DialogActions";
-import IconButton from "@material-ui/core/IconButton";
-import CloseIcon from "@material-ui/icons/Close";
-import Typography from "@material-ui/core/Typography";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import { AgChartsReact } from "ag-charts-react";
 
-const styles = (theme) => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(2),
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    width: 50,
+    minWidth: 120,
+    position: "relative",
+    left: "450px",
+    paddingTop: "10px",
   },
-  closeButton: {
-    position: "absolute",
-    right: theme.spacing(1),
-    top: theme.spacing(1),
-    color: theme.palette.grey[500],
-  },
-});
+}));
 
-const DialogTitle = withStyles(styles)((props) => {
-  const { children, classes, onClose, ...other } = props;
-  return (
-    <MuiDialogTitle disableTypography className={classes.root} {...other}>
-      <Typography variant="h6">{children}</Typography>
-      {onClose ? (
-        <IconButton
-          aria-label="close"
-          className={classes.closeButton}
-          onClick={onClose}
-        >
-          <CloseIcon />
-        </IconButton>
-      ) : null}
-    </MuiDialogTitle>
-  );
-});
-
-const DialogContent = withStyles((theme) => ({
-  root: {
-    padding: theme.spacing(2),
-  },
-}))(MuiDialogContent);
-
-const DialogActions = withStyles((theme) => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(1),
-  },
-}))(MuiDialogActions);
-
-export default function CustomizedDialogs() {
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
+export default function PieChart({ data, setPieChart }) {
+  const classes = useStyles();
+  const [open, setOpen] = useState();
+  const options = {
+    data,
+    series: [
+      {
+        type: "pie",
+        angleKey: "Count",
+        labelKey: "Score",
+      },
+    ],
   };
+
+  useEffect(() => {
+    setOpen(true);
+  }, []);
+
   const handleClose = () => {
     setOpen(false);
+    setPieChart(false);
   };
 
   return (
     <div>
-      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        Open dialog
-      </Button>
       <Dialog
         onClose={handleClose}
         aria-labelledby="customized-dialog-title"
         open={open}
       >
-        <DialogTitle id="customized-dialog-title" onClose={handleClose}>
-          Modal title
-        </DialogTitle>
-        <DialogContent dividers>
-          <Typography gutterBottom>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-            ac consectetur ac, vestibulum at eros.
-          </Typography>
-          <Typography gutterBottom>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
-            auctor.
-          </Typography>
-          <Typography gutterBottom>
-            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo
-            cursus magna, vel scelerisque nisl consectetur et. Donec sed odio
-            dui. Donec ullamcorper nulla non metus auctor fringilla.
-          </Typography>
-        </DialogContent>
-        <DialogActions>
-          <Button autoFocus onClick={handleClose} color="primary">
-            Save changes
-          </Button>
-        </DialogActions>
+        <FormControl variant="outlined" className={classes.formControl}>
+          <InputLabel htmlFor="outlined-age-native-simple">
+            Chart Format
+          </InputLabel>
+          <Select
+            native
+            value="Ten"
+            label="Age"
+            inputProps={{
+              name: "age",
+              id: "outlined-age-native-simple",
+            }}
+          >
+            <option value={10}>Ten</option>
+            <option value={20}>Twenty</option>
+            <option value={30}>Thirty</option>
+          </Select>
+        </FormControl>
+        <AgChartsReact options={options} />
       </Dialog>
     </div>
   );
